29907f6481e0578a2dff2aa758c9e43f41e27dda
Nastya Naumova <nastia.nnn005@gmail.com>
2023-05-30 13:38:02 +0300

    commit

---
+++ client/client.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 5555
+
+using namespace std;
+
+int main() {
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+    sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "172.17.0.2", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) 
+< 0) {
+        perror("Connect failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[1024] = {0};
+    int valread = read(sockfd, buffer, 1024);
+
+    cout << buffer << endl;
+
+    close(sockfd);
+    
+    return 0;
+}
+
---
+++ server/server.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+#define MAX_CLIENTS 10
+#define PORT 5555
+
+using namespace std;
+
+int main() {
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) 
+< 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sockfd, MAX_CLIENTS) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Server listening on port " << PORT << endl;
+    while(true) {
+    
+        int clientfd = accept(sockfd, NULL, NULL);
+        if (clientfd < 0) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        const char *message = "Wooohooo hello to whoever got the server down!!!";
+        send(clientfd, message, strlen(message), 0);
+
+        close(clientfd);
+    }
+
+    close(sockfd);
+
+    return 0;
+}
+
